2ce53c23db611eb3a2feb1e73e8e876854d9e0d7
NME.eth <nme@ethmail.cc>
2022-02-11 00:41:13 +0200

    changed configs to support hardhat-deploy plugin

--- contracts/Adidas/AdidasMinter.sol
+++ contracts/Adidas/AdidasMinter.sol
@@ -1,3 +1,4 @@
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
 import "hardhat/console.sol";
@@ -7,15 +8,14 @@ import "hardhat/console.sol";
 // @notice This contract mints and then transfers the tokens back to the caller
 // @dev This contract is meant to be called from a factory contract with the intent of mass mint nft tokens
 contract AdidasMinter {
-    address AdidasAddr = 0x28472a58A490c5e09A238847F66A68a47cC76f0f;
-    Adidas AdidasContract;
-    uint constant PRICE_PER_TOKEN = 0.2 ether;
+    address adidasAddr = 0x28472a58A490c5e09A238847F66A68a47cC76f0f;
+    Adidas adidasContract;
     
     // @notice Instantiating the adidasContract variable
     // @dev the contract needs to be seeded with enough eth to mint max tokens (hence payable)
     constructor() payable{
-        adidasContract = Adidas(adidasAddr);
-        adidasContract.mint{value: msg.value}(5);
+        //adidasContract = Adidas(adidasAddr);
+        //adidasContract.mint{value: msg.value}(5);
     }
 }
 
--- hardhat.config.ts
+++ hardhat.config.ts
@@ -7,8 +7,12 @@ import "@typechain/hardhat";
 import "hardhat-gas-reporter";
 import "solidity-coverage";
 import "hardhat-ethernal";
+import "hardhat-deploy";
 
 dotenv.config();
+
+let ALCHEMY_API_KEY=process.env.ALCHEMY_API_KEY;
+let DOODLES_FORK_BLOCKNUM=process.env.DOODLES_FORK_BLOCKNUM;
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
@@ -34,10 +38,15 @@ const config: HardhatUserConfig = {
     hardhat: {
       forking: {
         enabled: true,
-        url: `https://eth-mainnet.alchemyapi.io/v2/${process.env.ALCHEMY_API_KEY}`,
-        blockNumber: 13439086,
+        url: `https://eth-mainnet.alchemyapi.io/v2/${ALCHEMY_API_KEY}`,
+        blockNumber: Number(DOODLES_FORK_BLOCKNUM),
       },
     },
+  },
+  // config for the hardhat-deploy plugin
+  // allows us to name accounts and use in our deploy scripts
+  namedAccounts: {
+    deployer: 0, // setup to use the first account (index=0)
   },
   gasReporter: {
     enabled: process.env.REPORT_GAS !== undefined,
