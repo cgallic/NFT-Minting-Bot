50a8bc34b5d5558768041ea1205a52cd01fd5b4e
NME.eth <nme@ethmail.cc>
2022-02-13 23:17:47 +0200

    cleaned mint script with comments and variable names

--- scripts/Doodles/mint.ts
+++ scripts/Doodles/mint.ts
@@ -3,15 +3,27 @@
 //
 // When running the script with `npx hardhat run <script>` you'll find the Hardhat
 // Runtime Environment's members available in the global scope.
-import { ethers } from "hardhat";
+import { ethers, deployments, getNamedAccounts } from "hardhat";
 import "hardhat-ethernal";
 import hre from "hardhat";
 
 async function main() {
-  const artifacts = await hre.artifacts.readArtifact("DoodlesMinter");
-  console.log(artifacts);
-  const doodlesABI = artifacts.abi;
-  console.log(doodlesABI);
+  // Setting up constants related to Doodles minting function
+  const MINT_PER_CALL = 5; 
+  const MINT_PRICE = 0.123; 
+
+  // setting up the contract by linking it to the one we deployed
+  const doodlesMinter = await ethers.getContract("DoodlesMinter");
+  
+  // setting up params for the mint transaction
+  let mintCalls = 10; // how many times we want to call
+  let totalMintCost = mintCalls * MINT_PRICE * MINT_PER_CALL;
+  let options = {value: ethers.utils.parseEther(totalMintCost.toString())}
+  
+  // sending transaction 
+  let tx = await doodlesMinter.mint(10, options);
+  let receipt = await tx.wait();
+  console.log(receipt);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
@@ -20,4 +32,3 @@ main().catch((error) => {
   console.error(error);
   process.exitCode = 1;
 });
-
